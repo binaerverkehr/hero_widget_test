f5b35e37d3f94c174f46c2bd52dedad658595c55
Jay Ben <syntacops@gmail.com>
2022-10-17 10:49:02 +0200

    Recipe class completed

---
+++ lib/models/recipe.dart
@@ -0,0 +1,11 @@
+class Recipe {
+  final String title; // Titel
+  final String description; // Beschreibung
+  final List<String> ingredients; // Zutaten
+
+  Recipe(
+    this.title,
+    this.description,
+    this.ingredients,
+  );
+}
