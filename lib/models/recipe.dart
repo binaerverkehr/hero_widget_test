e7d513383624c748e51d31f3299e28dced2b157b
Jay Ben <syntacops@gmail.com>
2022-10-17 10:49:39 +0200

    Recipe class extended

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/material.dart';
+
 class Recipe {
   final String title; // Titel
   final String description; // Beschreibung
   final List<String> ingredients; // Zutaten
+  final Image image;
 
   Recipe(
     this.title,
     this.description,
     this.ingredients,
+    this.image,
   );
 }
